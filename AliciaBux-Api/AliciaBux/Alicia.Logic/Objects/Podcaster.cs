3d6321634f51333423d58f265ba63cb187397b6f
SantaPoneCentralDev <santaponecentraldev@gmail.com>
2021-04-09 15:17:37 -0600

    Logic objects

---
+++ AliciaBux-Api/AliciaBux/Alicia.Logic/Objects/Podcaster.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alicia.Logic.Objects
+{
+    public class Podcaster
+    {
+        public Guid podcasterID { get; set; }
+        public string podcasterName { get; set; }
+        public int podcasterBalance { get; set; }
+    }
+}
