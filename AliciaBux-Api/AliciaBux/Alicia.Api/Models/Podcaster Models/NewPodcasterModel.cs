c4284f81fd15385bf1fd6cffd2235ed2ffa4ccec
SantaPoneCentralDev <santaponecentraldev@gmail.com>
2021-04-09 15:19:25 -0600

    model bodies

---
+++ AliciaBux-Api/AliciaBux/Alicia.Api/Models/Podcaster Models/NewPodcasterModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alicia.Api.Models.Podcaster_Models
+{
+    public class NewPodcasterModel
+    {
+        public string name { get; set; }
+    }
+}
