8831cd028f62c520d51ed858be2d8facccf25423
SantaPoneCentralDev <santaponecentraldev@gmail.com>
2021-04-09 15:17:59 -0600

    Repo implimentation

---
+++ AliciaBux-Api/AliciaBux/Alicia.Data/Repository/Aliciatory.cs
@@ -0,0 +1,60 @@
+﻿using Alicia.Logic.Interfaces;
+using Alicia.Logic.Objects;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HiAliciaContext = Alicia.Data.Entities.HiAliciaContext;
+
+namespace Alicia.Data.Repository
+{
+    public class Aliciatory : IAliciatory
+    {
+        private readonly HiAliciaContext aliciaContext;
+
+        public Aliciatory(HiAliciaContext _context)
+        {
+            aliciaContext = _context ?? throw new ArgumentNullException(nameof(_context));
+        }
+
+        public async Task CreateNewPodcaster(string name, Guid newPodcasterGUID)
+        {
+            Entities.Podcaster contextPodcaster = new Entities.Podcaster()
+            {
+                PodcasterId = newPodcasterGUID,
+                PodcasterName = name,
+                PodcasterBalance = 0
+            };
+            await aliciaContext.Podcasters.AddAsync(contextPodcaster);
+        }
+
+        public async Task<List<Podcaster>> GetAllPodcasters()
+        {
+            List<Podcaster> logicPodcasters = await aliciaContext.Podcasters.Select(contextPodcaster => new Podcaster()
+            {
+                podcasterID = contextPodcaster.PodcasterId,
+                podcasterName = contextPodcaster.PodcasterName,
+                podcasterBalance = contextPodcaster.PodcasterBalance
+            }).ToListAsync();
+            return logicPodcasters;
+        }
+
+        public async Task<Podcaster> GetPodcasterByID(Guid podcasterID)
+        {
+            Podcaster logicPodcaster = await aliciaContext.Podcasters.Select(contextPodcaster => new Podcaster()
+            {
+                podcasterID = contextPodcaster.PodcasterId,
+                podcasterName = contextPodcaster.PodcasterName,
+                podcasterBalance = contextPodcaster.PodcasterBalance
+            }).AsNoTracking().FirstOrDefaultAsync(p => p.podcasterID == podcasterID);
+            return logicPodcaster;
+        }
+
+        public async Task SaveAsync()
+        {
+            await aliciaContext.SaveChangesAsync();
+        }
+    }
+}
