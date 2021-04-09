25050ec59af95e986aa9320dabb18a31a08228be
SantaPoneCentralDev <santaponecentraldev@gmail.com>
2021-04-09 12:39:18 -0600

    material libaries installed

--- AliciaBux-App/src/app/app.module.ts
+++ AliciaBux-App/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BankComponent } from './bank/bank.component';
+import { CustomMaterialModule } from './core/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BankComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CustomMaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
