a7e7810a8a45df56c04b6282d41e0d808cbeb4da
SantaPoneCentralDev <santaponecentraldev@gmail.com>
2021-04-09 15:17:30 -0600

    Alicia interface setup

---
+++ AliciaBux-Api/AliciaBux/Alicia.Logic/Interfaces/IAliciatory.cs
@@ -0,0 +1,30 @@
+﻿using Alicia.Logic.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alicia.Logic.Interfaces
+{
+    public interface IAliciatory
+    {
+        /// <summary>
+        /// Creates a new podcaster
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        Task CreateNewPodcaster(string name, Guid newPodcasterGUID);
+        /// <summary>
+        /// Returns a list of all podcaster info
+        /// </summary>
+        /// <returns></returns>
+        Task<List<Podcaster>> GetAllPodcasters();
+        Task<Podcaster> GetPodcasterByID(Guid podcasterID);
+        /// <summary>
+        /// Saves the context
+        /// </summary>
+        /// <returns></returns>
+        Task SaveAsync();
+    }
+}
